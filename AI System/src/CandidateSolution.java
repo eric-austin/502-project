d2d126be616faad09960824d19509de513af41de
eric-austin <eric.james.austin@gmail.com>
2019-11-05 12:00:53 -0700

    slight modification to candidate solution

--- AI System/src/CandidateSolution.java
+++ AI System/src/CandidateSolution.java
@@ -1,4 +1,10 @@
+/**
+ * Class representing a candidate solution to the optimization problem
+ * ie. it represents a specific policy mix and the fitness of that mix determined by
+ * an (external) objective function
+ */
 
 public class CandidateSolution {
-
+	//instance variables
+	private float fitness = (float) 0.0;
 }
