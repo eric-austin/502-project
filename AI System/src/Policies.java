b66b31a176faf509c3f58b31e7ab274e496d4842
Eric Austin <ericaustin@Erics-MacBook-Pro.local>
2019-11-06 15:51:57 -0700

    transportation, building and appliances policies done

--- AI System/src/Policies.java
+++ AI System/src/Policies.java
@@ -35,7 +35,7 @@ public class Policies {
 	static public Policy FrgtTDM = new Policy("Fraction of TDM Package Implemented[freight]", real, 0.0, 1.0);
 	static public Policy LCFS = new Policy("Additional LCFS Percentage", real, 0.0, 0.4);
 	
-	//building and appliances sector policies
+	//building and appliances sector policies (36)
 	static public Policy BuildingElecUrban = new Policy("Percent New Nonelec Component Sales Shifted to Elec[urban residential]", real, 0.0, 1.0);
 	static public Policy BuildingElecRural = new Policy("Percent New Nonelec Component Sales Shifted to Elec[rural residential]", real, 0.0, 1.0);
 	static public Policy BuildingElecComm = new Policy("Percent New Nonelec Component Sales Shifted to Elec[commercial]", real, 0.0, 1.0);
@@ -48,11 +48,45 @@ public class Policies {
 	static public Policy BuildingEffUrbOther = new Policy("Reduction in E Use Allowed by Component Eff Std[other component,urban residential]", real, 0.0, 1.0);
 	static public Policy BuildingEffRurHeat = new Policy("Reduction in E Use Allowed by Component Eff Std[heating,rural residential]", real, 0.0, 1.0);
 	static public Policy BuildingEffRurCool = new Policy("Reduction in E Use Allowed by Component Eff Std[cooling and ventilation,rural residential]", real, 0.0, 1.0);
+	static public Policy BuildingEffRurEnv = new Policy("Reduction in E Use Allowed by Component Eff Std[envelope,rural residential]", real, 0.0, 1.0);
+	static public Policy BuildingEffRurLight = new Policy("Reduction in E Use Allowed by Component Eff Std[lighting,rural residential]", real, 0.0, 1.0);
+	static public Policy BuildingEffRurApp = new Policy("Reduction in E Use Allowed by Component Eff Std[appliances,rural residential]", real, 0.0, 1.0);
+	static public Policy BuildingEffRurOther = new Policy("Reduction in E Use Allowed by Component Eff Std[other component,rural residential", real, 0.0, 1.0);
+	static public Policy BuildingEffCommHeat = new Policy("Reduction in E Use Allowed by Component Eff Std[heating,commercial]", real, 0.0, 1.0);
+	static public Policy BuildingEffCommCool = new Policy("Reduction in E Use Allowed by Component Eff Std[cooling and ventilation,commercial]", real, 0.0, 1.0);
+	static public Policy BuildingEffCommEnv = new Policy("Reduction in E Use Allowed by Component Eff Std[envelope,commercial]", real, 0.0, 1.0);
+	static public Policy BuildingEffCommLight = new Policy("Reduction in E Use Allowed by Component Eff Std[lighting,commercial]", real, 0.0, 1.0);
+	static public Policy BuildingEffCommApp = new Policy("Reduction in E Use Allowed by Component Eff Std[appliances,commercial]", real, 0.0, 1.0);
+	static public Policy BuildingEffCommOther = new Policy("Reduction in E Use Allowed by Component Eff Std[other component,commercial]", real, 0.0, 1.0);
+	
+	static public Policy ContrTraining = new Policy("Boolean Improved Contractor Edu and Training", bool, 0.0, 1.0);
+	
+	static public Policy SolarCarveOut = new Policy("Min Fraction of Total Elec Demand to be Met by Distributed Solar PV", real, 0.0, 0.24);
+	
+	static public Policy SolarSubsidy = new Policy("Perc Subsidy for Distributed Solar PV Capacity", real, 0.0, 0.5);
+	
+	static public Policy ImprovedLabel = new Policy("Boolean Improved Device Labeling", bool, 0.0, 1.0);
+	
+	static public Policy RetroHeat = new Policy("Fraction of Commercial Components Replaced Annually due to Retrofitting Policy[heating]", real, 0.0, 0.034);
+	static public Policy RetroCool = new Policy("Fraction of Commercial Components Replaced Annually due to Retrofitting Policy[cooling and ventilation]", real, 0.0, 0.034);
+	static public Policy RetroEnv = new Policy("Fraction of Commercial Components Replaced Annually due to Retrofitting Policy[envelope]", real, 0.0, 0.034);
+	static public Policy RetroLight = new Policy("Fraction of Commercial Components Replaced Annually due to Retrofitting Policy[lighting]", real, 0.0, 0.034);
+	static public Policy RetroApp = new Policy("Fraction of Commercial Components Replaced Annually due to Retrofitting Policy[appliances]", real, 0.0, 0.034);
+	static public Policy RetroOther = new Policy("Fraction of Commercial Components Replaced Annually due to Retrofitting Policy[other component]", real, 0.0, 0.034);
+	
+	static public Policy RebateHeat = new Policy("Boolean Rebate Program for Efficient Components[heating]", bool, 0.0, 1.0);
+	static public Policy RebateCool = new Policy("Boolean Rebate Program for Efficient Components[cooling and ventilation]", bool, 0.0, 1.0);
+	static public Policy RebateEnv = new Policy("Boolean Rebate Program for Efficient Components[envelope]", bool, 0.0, 1.0);
+	static public Policy RebateLight = new Policy("Boolean Rebate Program for Efficient Components[lighting]", bool, 0.0, 1.0);
+	static public Policy RebateApp = new Policy("Boolean Rebate Program for Efficient Components[appliances]", bool, 0.0, 1.0);
 	
 	//vector of all policies to be referenced by candidate solutions
 	static public Policy[] allPolicies = {PollutionLDVs, PollutionHDVs, PollutionAircraft, PollutionRail, PollutionShips, PollutionMtrbks, EVPerks, 
 			Feebate, EVSalesPassengerLDVs, EVSalesFreightLDVs, EVSalesPassengerHDVs, EVSalesFreightHDVs, EVSalesPassengerMtrbks,EVSubsidyPassengerLDVs, 
 			EVSubsidyFreightHDVs, FuelEconLDVs, FuelEconHDVs, FuelEconAircraft, FuelEconRail, FuelEconShips, FuelEconMtrbks, PsgrTDM, FrgtTDM, LCFS,
 			BuildingElecUrban, BuildingElecRural, BuildingElecComm, BuildingEffUrbHeat, BuildingEffUrbCool, BuildingEffUrbEnv, BuildingEffUrbLight,
-			BuildingEffUrbApp, BuildingEffUrbOther, BuildingEffRurHeat, BuildingEffRurCool};
+			BuildingEffUrbApp, BuildingEffUrbOther, BuildingEffRurHeat, BuildingEffRurCool, BuildingEffRurEnv, BuildingEffRurLight, BuildingEffRurApp,
+			BuildingEffRurOther, BuildingEffCommHeat, BuildingEffCommCool, BuildingEffCommEnv, BuildingEffCommLight, BuildingEffCommApp, BuildingEffCommOther,
+			ContrTraining, SolarCarveOut, SolarSubsidy, ImprovedLabel, RetroHeat, RetroCool, RetroEnv, RetroLight, RetroApp, RetroOther, RebateHeat,
+			RebateCool, RebateEnv, RebateLight, RebateApp};
 }
